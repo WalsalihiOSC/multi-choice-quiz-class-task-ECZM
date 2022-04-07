2213c8ca54971e3d1e54bd53c53d04601c717971
Ella Ma <84163020+ECZM@users.noreply.github.com>
2022-04-08 09:28:37 +1200

    Updated file - MultiChoiceQuiz.py
    Improving the code.
    Created an instance of the Quiz class.
    Moved the questions and choices to Quiz class.

--- MultiChoiceQuiz.py
+++ MultiChoiceQuiz.py
@@ -7,53 +7,48 @@ class MultiChoiceQuiz:
 
     def __init__(self, parent):
         self.answers = []
+        self.q = Quiz()
 
-        f1 = Frame(parent, bg = "#CFE2F3", height = 300, width = 300)
+        f1 = Frame(parent, bg = "#CFE2F3", height = 20, width = 300)
         f1.grid(column = 0, row = 0)
-
         f1.grid_propagate(0)
 
-        label1 = Label(f1, text = "Question:", bg = "#CFE2F3")
+        f2 = Frame(parent, bg = "#CFE2F3", height = 300, width = 300)
+        f2.grid(column = 0, row = 1)
+        f2.grid_propagate(0)
+
+        label1 = Label(f1, text = "Question: {}".format(self.q.question), bg = "#CFE2F3")
         label1.grid(column = 0, row = 0)
 
-        label2 = Label(f1, text = "What is the capital of Mongolia?", bg = "#CFE2F3")
+        label2 = Label(f2, text = "", bg = "#CFE2F3")
         label2.grid(column = 1, row = 0)
 
         self.v = StringVar()
         self.v.set("0")
-        choice = ["Vladivostok", "Astana", "Ulaanbaatar", "Lhasa"]
         self.choices = []
-        for i in range(len(choice)):
-            self.choices.append(Radiobutton(f1, variable = self.v, value = choice[i], text = choice[i], bg = "#CFE2F3", command = self.configure_answer))
-            self.choices[i].grid(column = 1, row = i+1, sticky = W, padx = 20)
+        for i in range(len(self.q.choices)):
+            self.choices.append(Radiobutton(f2, variable = self.v, value = self.q.choices[i], text = self.q.choices[i], bg = "#CFE2F3", command = self.configure_answer))
+            self.choices[i].grid(row = i+1, sticky = W, padx = 50)
 
-        self.label3 = Label(f1, text = "", bg = "#CFE2F3")
-        self.label3.grid(column = 0, row = 5)
-
-        self.answers.append(Validate(self.v.get()))
+        self.label3 = Label(f2, text = "", bg = "#CFE2F3")
+        self.label3.grid(column = 0, row = 5, sticky = W)
 
     def configure_answer(self):
-        answer = 0
-        v = Validate(answer)
-        if v.check_answer == True:
+        if self.q.check_answer(self.v.get()):
             self.label3.configure(text = "Correct!")
         else:
             self.label3.configure(text = "*Incorrect")
 
 
+class Quiz:
+    def __init__(self):
+        self.question = "What is the capital of Mongolia?"
+        self.choices = ["Vladivostok", "Astana", "Ulaanbaatar", "Lhasa"]
 
-
-class Validate(MultiChoiceQuiz):
-    def __init__(self, answer):
-        self.answer = answer
-
-    def check_answer(self):
-        if self.answer == "Ulaanbaatar":
+    def check_answer(self, answer):
+        if answer == "Ulaanbaatar":
             return True
-        else:
-            return False
-
-
+        return False
 
 
 
